6b00f60e126b3567bf6d8f4bdf2c0d52853857fc
Tsaliki eswararo <tsalikieswararo@tsalikis-macbook-pro.local>
2017-04-10 12:07:20 +0530

    Initial Commit

---
+++ ExProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExProject
+//
+//  Created by Tsaliki eswararo on 4/10/17.
+//  Copyright © 2017 Chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExProject
+//
+//  Created by Tsaliki eswararo on 4/10/17.
+//  Copyright © 2017 Chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
